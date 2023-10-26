2ad56ca76199cc2d72dc11799180617367374516
Atuh Samuel <atuhsamuel96@gmail.com>
2023-10-26 15:25:50 +0100

    Updates

--- sfacil/urls.py
+++ sfacil/urls.py
@@ -17,11 +17,13 @@ Including another URLconf
 from xml.etree.ElementInclude import include
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from rest_framework.response import Response
 
 
 @api_view(['GET'])
+@authentication_classes([])
+@permission_classes([])
 def home(request):
     return Response({"success": True, "message": "Hello World🎊❤️"})
 
